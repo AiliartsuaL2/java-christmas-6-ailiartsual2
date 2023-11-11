29a131b367f929201003b9f65a29c2fc7159dab1
ailiartsua <ailiartsual2@gmail.com>
2023-11-11 15:57:50 +0900

    feat : 할인 내역 domain 정의
    - 크리스마스 디데이 할인, 평일,주말 할인, 특별 할인, 증정 이벤트 여부에 대한 필드 정의

---
+++ src/main/java/christmas/domain/Discount.java
@@ -0,0 +1,8 @@
+package christmas.domain;
+
+public class Discount {
+    private int christmasDiscount;
+    private int dayDiscount;
+    private int specialDiscount;
+    private boolean isPresent;
+}
