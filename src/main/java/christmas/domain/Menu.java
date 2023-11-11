38572f039c339182ed139faa492a04f9814995ce
ailiartsua <ailiartsual2@gmail.com>
2023-11-11 15:30:48 +0900

    feat : 주문 메뉴 enum 정의
    - 주문 메뉴를 타입, 메뉴명, 가격으로 정의
    - 주문 메뉴 내부에 private enum 선언하여 코스 타입 설정

---
+++ src/main/java/christmas/domain/Menu.java
@@ -0,0 +1,39 @@
+package christmas.domain;
+
+public enum Menu {
+    MUSHROOM_SOUP(CourseType.APPETIZER, "양송이수프", 6000),
+    TAPAS(CourseType.APPETIZER, "타파스", 5500),
+    CAESAR_SALAD(CourseType.APPETIZER, "시저샐러드", 8000),
+    T_BONE_STEAK(CourseType.MAIN, "티본스테이크", 55000),
+    BARBECUE_RIBS(CourseType.MAIN, "바베큐립", 54000),
+    SEA_FOOD_PASTA(CourseType.MAIN, "해산물파스타", 35000),
+    CHRISTMAS_PASTA(CourseType.MAIN, "크리스마스파스타", 25000),
+    CHOCOLATE_CAKE(CourseType.DESSERT, "초코케이크", 15000),
+    ICE_CREAM(CourseType.DESSERT, "아이스크림", 5000),
+    ZERO_COKE(CourseType.BEVERAGE, "제로콜라", 3000),
+    RED_WINE(CourseType.BEVERAGE, "레드와인", 60000),
+    CHAMPAGNE(CourseType.BEVERAGE, "샴페인", 25000);
+
+    Menu(CourseType courseType, String name, int price) {
+        this.courseType = courseType;
+        this.name = name;
+        this.price = price;
+    }
+
+    private final CourseType courseType;
+    private final String name;
+    private final int price;
+
+    private enum CourseType {
+        APPETIZER("에피타이저"),
+        MAIN("메인"),
+        DESSERT("디저트"),
+        BEVERAGE("음료");
+
+        private final String name;
+
+        CourseType(String name) {
+            this.name = name;
+        }
+    }
+}
