399a6c1da108d2630473c4f7e4f25f91e16fed87
ailiartsua <ailiartsual2@gmail.com>
2023-11-11 15:52:41 +0900

    feat : 이벤트 배지 enum 정의
    - 이벤트 배지에 대한 enum 정의
    - 배지명, 최소 금액에 대한 필드 정의
    - 총 혜택 금액을 통한 팩토리 메서드 제공

---
+++ src/main/java/christmas/domain/EventBadge.java
@@ -0,0 +1,26 @@
+package christmas.domain;
+
+public enum EventBadge {
+    NOT_EXIST("없음", 0),
+    STAR("별", 5000),
+    TREE("트리", 10000),
+    SANTA("산타", 20000);
+
+    private final String name;
+    private final int minimumPrice;
+
+    EventBadge(String name, int minimumPrice) {
+        this.name = name;
+        this.minimumPrice = minimumPrice;
+    }
+
+    public static EventBadge getEventBadge(int totalBenefitAmount) {
+        if(totalBenefitAmount >= SANTA.minimumPrice) {
+            return SANTA;
+        } if(totalBenefitAmount >= TREE.minimumPrice) {
+            return TREE;
+        } if(totalBenefitAmount >= STAR.minimumPrice) {
+            return STAR;
+        } return NOT_EXIST;
+    }
+}
